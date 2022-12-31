9b4bcd8341bbc95447042bc96c0b4c8c646b51de
(hunk!nd3 p4d4y41! <68646925+chankintepadayali@users.noreply.github.com>
2022-12-31 20:22:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Readline = require('readline')
 const yargs = require('yargs/yargs')
 const rl = Readline.createInterface(process.stdin, process.stdout)
 
-CFonts.say('SimpleBot - MD', {
+CFonts.say('LIZAMWOL3.0-MD', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
