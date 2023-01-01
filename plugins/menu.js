95e8dbe7ae6237b4d5208ddd39b47b247b68dbdc
(hunk!nd3 p4d4y41! <68646925+chankintepadayali@users.noreply.github.com>
2023-01-01 17:10:01 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -9,72 +9,72 @@ let menu = `
 ║≡≡≡≡≡≡≡≡≡≡≡≡≡≡
 ╰══╡✯✯✯✯✯✯✯✯╞══╯
 ┏━━━━━━━━━━━━━┓
-┃ *< COMANDOS >*
+┃ *< COMANDS >*
 ┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-┣ ඬ⃟💫 _a_
-┣ ඬ⃟💫 _${usedPrefix}cat_
-┣ ඬ⃟💫 _${usedPrefix}dog_
-┣ ඬ⃟💫 _${usedPrefix}meme_
-┣ ඬ⃟💫 _${usedPrefix}logos_
-┣ ඬ⃟💫 _${usedPrefix}runtime_
-┣ ඬ⃟💫 _${usedPrefix}infohost_
-┣ ඬ⃟💫 _${usedPrefix}on welcome_
-┣ ඬ⃟💫 _${usedPrefix}off welcome_
-┣ ඬ⃟💫 _${usedPrefix}ig *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}mediafire *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}tiktok *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}sticker *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}attp *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}attp2 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}attp3 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ttp *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ttp2 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ttp3 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ttp4 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ttp5 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}wikipedia *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}google *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}imagen *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}play *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}play2 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}play3 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}invocar *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ytsearch *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}getaud *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}getvid *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}ytmp3 *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}ytmp4 *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}tts *[lenguaje] [texto]*_
-┣ ඬ⃟💫 _${usedPrefix}toimg *[sticker]*_
-┣ ඬ⃟💫 _${usedPrefix}sticker *[imagen]*_
-┣ ඬ⃟💫 _${usedPrefix}tourl *[imagen]*_
-┣ ඬ⃟💫 _${usedPrefix}tourl *[video]*_
-┣ ඬ⃟💫 _${usedPrefix}tourl *[audio]*_
-┣ ඬ⃟💫 _${usedPrefix}fat *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}bass *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}blown *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}deep *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}fast *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}robot *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}slow *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}tupai *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}vibra *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}nightcore *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}earrape *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}reverse *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}smooth *[nota de voz]*_
+┣ ඬ⃟. _a_
+┣ ඬ⃟. _${usedPrefix}cat_
+┣ ඬ⃟. _${usedPrefix}dog_
+┣ ඬ⃟. _${usedPrefix}meme_
+┣ ඬ⃟. _${usedPrefix}logos_
+┣ ඬ⃟. _${usedPrefix}runtime_
+┣ ඬ⃟. _${usedPrefix}infohost_
+┣ ඬ⃟. _${usedPrefix}on welcome_
+┣ ඬ⃟. _${usedPrefix}off welcome_
+┣ ඬ⃟. _${usedPrefix}ig *[url]*_
+┣ ඬ⃟. _${usedPrefix}mediafire *[url]*_
+┣ ඬ⃟. _${usedPrefix}tiktok *[url]*_
+┣ ඬ⃟. _${usedPrefix}sticker *[url]*_
+┣ ඬ⃟. _${usedPrefix}attp *[texto]*_
+┣ ඬ⃟. _${usedPrefix}attp2 *[texto]*_
+┣ ඬ⃟. _${usedPrefix}attp3 *[texto]*_
+┣ ඬ⃟. _${usedPrefix}ttp *[texto]*_
+┣ ඬ⃟. _${usedPrefix}ttp2 *[texto]*_
+┣ ඬ⃟. _${usedPrefix}ttp3 *[texto]*_
+┣ ඬ⃟. _${usedPrefix}ttp4 *[texto]*_
+┣ ඬ⃟. _${usedPrefix}ttp5 *[texto]*_
+┣ ඬ⃟. _${usedPrefix}wikipedia *[texto]*_
+┣ ඬ⃟. _${usedPrefix}google *[texto]*_
+┣ ඬ⃟. _${usedPrefix}imagen *[texto]*_
+┣ ඬ⃟. _${usedPrefix}play *[texto]*_
+┣ ඬ⃟. _${usedPrefix}play2 *[texto]*_
+┣ ඬ⃟. _${usedPrefix}play3 *[texto]*_
+┣ ඬ⃟. _${usedPrefix}invocar *[texto]*_
+┣ ඬ⃟. _${usedPrefix}ytsearch *[texto]*_
+┣ ඬ⃟. _${usedPrefix}getaud *[url]*_
+┣ ඬ⃟. _${usedPrefix}getvid *[url]*_
+┣ ඬ⃟. _${usedPrefix}ytmp3 *[url]*_
+┣ ඬ⃟. _${usedPrefix}ytmp4 *[url]*_
+┣ ඬ⃟. _${usedPrefix}tts *[lenguaje] [texto]*_
+┣ ඬ⃟. _${usedPrefix}toimg *[sticker]*_
+┣ ඬ⃟. _${usedPrefix}sticker *[imagen]*_
+┣ ඬ⃟. _${usedPrefix}tourl *[imagen]*_
+┣ ඬ⃟. _${usedPrefix}tourl *[video]*_
+┣ ඬ⃟. _${usedPrefix}tourl *[audio]*_
+┣ ඬ⃟. _${usedPrefix}fat *[nota de voz]*_
+┣ ඬ⃟. _${usedPrefix}bass *[nota de voz]*_
+┣ ඬ⃟. _${usedPrefix}blown *[nota de voz]*_
+┣ ඬ⃟. _${usedPrefix}deep *[nota de voz]*_
+┣ ඬ⃟. _${usedPrefix}fast *[nota de voz]*_
+┣ ඬ⃟. _${usedPrefix}robot *[nota de voz]*_
+┣ ඬ⃟. _${usedPrefix}slow *[nota de voz]*_
+┣ ඬ⃟. _${usedPrefix}tupai *[nota de voz]*_
+┣ ඬ⃟. _${usedPrefix}vibra *[nota de voz]*_
+┣ ඬ⃟. _${usedPrefix}nightcore *[nota de voz]*_
+┣ ඬ⃟. _${usedPrefix}earrape *[nota de voz]*_
+┣ ඬ⃟. _${usedPrefix}reverse *[nota de voz]*_
+┣ ඬ⃟. _${usedPrefix}smooth *[nota de voz]*_
 ┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
 ┃ *< OWNER >*
 ┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-┣ ඬ⃟💫 _${usedPrefix}update_
-┣ ඬ⃟💫 _${usedPrefix}banchat_
-┣ ඬ⃟💫 _${usedPrefix}unbanchat_
+┣ ඬ⃟. _${usedPrefix}update_
+┣ ඬ⃟. _${usedPrefix}banchat_
+┣ ඬ⃟. _${usedPrefix}unbanchat_
 ┗━━━━━━━━━━━━━┛
 `.trim()
 let buttons = [
-{ buttonId: '#owner', buttonText: { displayText: '💫 ᴏᴡɴᴇʀ💫' }, type: 1 },
-{ buttonId: '#runtime', buttonText: { displayText: '⏰ ʀᴜɴ ᴛɪᴍᴇ ⏰' }, type: 1 },
-{ buttonId: '#infohost', buttonText: { displayText: '👑 ɪɴꜰᴏ ʜᴏꜱᴛ 👑' }, type: 1 }]
+{ buttonId: '#owner', buttonText: { displayText: 'ᴏᴡɴᴇʀ' }, type: 1 },
+{ buttonId: '#runtime', buttonText: { displayText: 'ʀᴜɴ ᴛɪᴍᴇ' }, type: 1 },
+{ buttonId: '#infohost', buttonText: { displayText: 'ɪɴꜰᴏ ʜᴏꜱᴛ' }, type: 1 }]
 let buttonMessage = {
 image: fs.readFileSync('./Menu2.jpg'),
 caption: menu.trim(),
@@ -88,7 +88,7 @@ externalAdReply: {
 showAdAttribution: true,
 mediaType: 'VIDEO',
 mediaUrl: null,
-title: '👑 ᴛᴇʀᴍᴜx ʜᴏꜱᴛ👑',
+title: 'ᴛᴇʀᴍᴜx ʜᴏꜱᴛ',
 body: null,
 thumbnail: fs.readFileSync('./src/logo.png'),
 sourceUrl: `https://chat.whatsapp.com/F0fU7LSlBBcBm6ny5fVSuT`
