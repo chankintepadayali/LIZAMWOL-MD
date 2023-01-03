288b49504ad78f9e99e9f97fd5dd5d12e467c696
(hunk!nd3 p4d4y41! <68646925+chankintepadayali@users.noreply.github.com>
2023-01-03 12:28:21 +0530

    Delete plugins directory

--- plugins/infohost.js
+++
@@ -1,7 +0,0 @@
-let handler = async (m, { conn }) => {
-m.reply(` information empty...!
-host details & more features soon 😽ī.am/ꪶchankinte_padayaliꫂ
-`)
-}
-handler.command = /^(infohost)$/i
-module.exports = handler
